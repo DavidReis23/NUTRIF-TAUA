485ec5256980140a864c8bb84898da0f9026de27
David Reis <davidsilvareis23@gmail.com>
2026-03-13 11:08:26 -0300

    Reorganização das pastas e códigos na Nutri

--- sisref-react/src/App.jsx
+++ sisref-react/src/App.jsx
@@ -3,20 +3,20 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
 // Importando as nossas telas:
-import Dashboard from "./pages/Dashboard";
-import Cardapio from "./pages/Cardapio";
-import AddCardapio from "./pages/AddCardapio";
-import Cursos from "./pages/Cursos";
-import AddCurso from "./pages/AddCurso";
-import Alunos from "./pages/Alunos";
-import AddAluno from "./pages/AddAluno";
-import QrCode from "./pages/QrCode";
-import QrCodeLista from "./pages/QrCodeLista";
-import Bolsista from "./pages/Bolsista";
-import Relatorios from "./pages/Relatorios";
-import RelatorioAuditoria from "./pages/RelatorioAuditoria";
-import RelatorioPresenca from "./pages/RelatorioPresenca";
-import Notificacoes from "./pages/Notificacoes";
+import Dashboard from "./pages/pageNutri/Dashboard";
+import Cardapio from "./pages/pageNutri/Cardapio";
+import AddCardapio from "./pages/pageNutri/AddCardapio";
+import Cursos from "./pages/pageNutri/Cursos";
+import AddCurso from "./pages/pageNutri/AddCurso";
+import Alunos from "./pages/pageNutri/Alunos";
+import AddAluno from "./pages/pageNutri/AddAluno";
+import QrCode from "./pages/pageNutri/QrCode";
+import QrCodeLista from "./pages/pageNutri/QrCodeLista";
+import Bolsista from "./pages/pageNutri/Bolsista";
+import Relatorios from "./pages/pageNutri/Relatorios";
+import RelatorioAuditoria from "./pages/pageNutri/RelatorioAuditoria";
+import RelatorioPresenca from "./pages/pageNutri/RelatorioPresenca";
+import Notificacoes from "./pages/pageNutri/Notificacoes";
 
 function App() {
   return (
